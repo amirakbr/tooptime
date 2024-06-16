3e4d92381af9798c3e0111ca3110b21943dc9f68
haj amir akbarzadeh <amir.akbrzadh3@gmail.com>
2024-06-16 16:58:09 +0430

    tsx : feature = add hero page slider images

--- app/component/landingPage/heroSlider.tsx
+++ app/component/landingPage/heroSlider.tsx
@@ -1,12 +1,29 @@
-"use client";
-import { register } from "swiper/element/bundle";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import "swiper/css";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+'use client';
+import { register } from 'swiper/element/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import 'swiper/css';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { text } from 'stream/consumers';
+import Image from 'next/image';
 
 register();
 SwiperCore.use([Autoplay, Pagination, Navigation]);
+
+const sliderConfigData = [
+  {
+    img: '/football.png',
+    text: 'با کیفیت ترین زمین های فوتبال سرپوشیده شده',
+  },
+  {
+    img: '/football.png',
+    text: 'با کیفیت ترین زمین های فوتبال سرپوشیده شده',
+  },
+  {
+    img: '/football.png',
+    text: 'با کیفیت ترین زمین های فوتبال سرپوشیده شده',
+  },
+];
 
 const HeroSlider = () => {
   return (
@@ -15,7 +32,7 @@ const HeroSlider = () => {
       spaceBetween={30}
       loop={true}
       autoplay={{
-        delay: 2500,
+        delay: 250000,
         disableOnInteraction: false,
       }}
       pagination={{
@@ -23,10 +40,18 @@ const HeroSlider = () => {
       }}
       navigation={true}
       className="w-full h-full"
+      onSlideChange={(e) => {
+        console.log(e, 'e');
+      }}
     >
-      <SwiperSlide className="bg-slate-500 relative">testr</SwiperSlide>
-      <SwiperSlide className="bg-slate-500 relative">testr</SwiperSlide>
-      <SwiperSlide className="bg-slate-500 relative">testr</SwiperSlide>
+      {sliderConfigData.map(({ img, text }) => (
+        <SwiperSlide className="bg-slate-500 relative max-h-[30rem] relative">
+          <span className='text-xl'>
+
+          </span>
+          <Image width={1000} height={1000} src={img} alt={'tst'} className="object-cover object-center !w-full !h-[30rem]" />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
